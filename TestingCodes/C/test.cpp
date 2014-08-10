96be18e848dc21a8f69636dea75181c723632fd3
miao <miao@miao-Latitude-E6520.(none)>
2014-08-10 23:45:18 +0200

    more

--- TestingCodes/C/test.cpp
+++ TestingCodes/C/test.cpp
@@ -1,9 +1,16 @@
 # include <iostream>
 using namespace std;
 
+double msquare(double );
+
 int main(){
 	
-	cout<< "Hello World!!"<<endl;
+	cout<< "Hello World!!"<< msquare(6) << endl;
 
 	return 0;
+}
+
+double msquare(double x)
+{
+	return x*x;
 }
