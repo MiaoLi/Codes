a400149442cd46355437d4a547def8cfed564f9c
miao <miao@miao-Latitude-E6520.(none)>
2014-08-12 22:21:07 +0200

    new

--- TestingCodes/C/test_1_7.cpp
+++ TestingCodes/C/test_1_7.cpp
@@ -1,16 +1,9 @@
 # include <iostream>
-// use to inuput the time
+# include <string>
+
 using namespace std;
 int main(){
 
-int hours;
-int minutes;
-
-cout<<"Enter the number of hours: ";
-cin>>hours;
-cout<<"Enter the number of minutes: ";
-cin >> minutes;
-
-cout<< "Time: "<<hours<<":"<<minutes<<endl;
-
+	 
+	return 0;
 }
